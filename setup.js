ed6c47eb1f37fd53591efdb91878a5a0d26bee65
intellild <intellild@outlook.com>
2023-05-23 16:31:11 +0800

    setup script

---
+++ setup.js
@@ -0,0 +1,45 @@
+const os = require("node:os");
+const shell = require("shelljs");
+const prettier = require("prettier");
+const fs = require("node:fs");
+const path = require("node:path");
+const { program } = require("commander");
+
+program
+  .option("-s, --source <source>", "runner archive")
+  .option("-t, --token <token>")
+  .option("-u, --url <url>")
+  .option("-n, --count <count>", "number of runner", (value) => Number(value));
+
+program.parse();
+
+const { source, token, url, count } = program.opts();
+console.log(program.opts());
+const args = `--unattended --url ${url} --token ${token}`;
+
+const prefix = os.hostname();
+
+const apps = [];
+
+shell.config.silent = false;
+shell.config.fatal = true;
+
+for (let i = 0; i < count; i++) {
+  const dir = `runner-${i}`;
+  shell.exec(`tar xzf ${source} -C ${dir}`);
+  shell.pushd(dir);
+  shell.exec(`./config.sh remove --token ${token}`);
+  shell.exec(`./config.sh ${args} --name ${prefix}-${i}`);
+  shell.popd();
+  apps.push({
+    name: dir,
+    cwd: dir,
+    script: `run.sh`,
+    interpreter: "/bin/bash",
+    exec_mode: "fork",
+    watch: false,
+  });
+}
+
+const config = prettier.format(JSON.stringify({ apps }), { parser: "json" });
+fs.writeFileSync(path.resolve(__dirname, "config.generated.json"), config);
